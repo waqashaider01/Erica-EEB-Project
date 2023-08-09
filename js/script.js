00c0dd832b35bba67f338af4adecfda5d6f4489b
sobia001 <09799.exe@gmail.com>
2023-08-09 14:23:36 +0500

    ok

---
+++ js/script.js
@@ -0,0 +1,27 @@
+
+$.fn.jQuerySimpleCounter = function (options) {
+    var settings = $.extend({
+        start: 0,
+        end: 100,
+        easing: 'swing',
+        duration: 400,
+        complete: ''
+    }, options);
+
+    var thisElement = $(this);
+
+    $({ count: settings.start }).animate({ count: settings.end }, {
+        duration: settings.duration,
+        easing: settings.easing,
+        step: function () {
+            var mathCount = Math.ceil(this.count);
+            thisElement.text(mathCount);
+        },
+        complete: settings.complete
+    });
+};
+
+
+$('#number1').jQuerySimpleCounter({ end: 20, duration: 3000 });
+$('#number2').jQuerySimpleCounter({ end: 7079, duration: 3000 });
+$('#number3').jQuerySimpleCounter({ end: 1043, duration: 2000 });
